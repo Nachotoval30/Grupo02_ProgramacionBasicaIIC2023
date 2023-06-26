4f2bc63a250970ac22acc4fc7c6902d94b2ac80c
nachotoval30 <nachotoval.12@outlook.com>
2023-06-25 20:28:21 -0600

    borrar unas cosas

--- Principal_Dreamworld_Casino.py
+++ Principal_Dreamworld_Casino.py
@@ -18,19 +18,18 @@ while opcion != 4:
 
     if opcion == 1:
         print("Ha seleccionado Registro de usuario nuevo")
-        # Aquí puedes agregar el código para el registro de nuevos usuarios
+        
 
     elif opcion == 2:
         print("Ha seleccionado DreamWorld Casino")
-        # Aquí puedes agregar el código para el casino
-
+        
     elif opcion == 3:
         print("Ha seleccionado Configuración Avanzada")
-        # Aquí puedes agregar el código para la configuración avanzada
+      
 
     elif opcion == 4:
         print("Ha seleccionado Salir")
-        # Aquí puedes agregar el código para finalizar el programa
+     
 
     else:
         print("Opción inválida. Por favor, seleccione una opción válida.")
