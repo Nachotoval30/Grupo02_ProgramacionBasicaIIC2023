52e7613d1ca12c0273ae1cd46a22acb50378577e
nachotoval30 <nachotoval.12@outlook.com>
2023-07-02 16:07:53 -0600

    modificacion de registro de ususario

--- Registro_de_Usuario.py
+++ Registro_de_Usuario.py
@@ -1,26 +1,64 @@
 #Creacion de nombre de ususraio
-
-def obtener_id():
+import getpass
+print("Bienvenido al resgistro de ususario")
+valor_minimo = 200
+dolares = 1
+colones = 536
+bitcoin = 30590
+deposto_usuario = list[] 
+def registro_usuario():
     intentos = 0
-    while intentos < 3:
-        ID = input("Ingrese un ID alfanumerico de al menos 5 cararcteres: ")
-        if len(ID) >= 5 and ID.isalnum():
-           return ID
-        else:
-            print("El ID ingresado debe tener 5 caracteres, Intentelo de Nuevo")
-            intentos += 1
-    print("Has excedido el número máximo de intentos. Regresando al menú principal.")
    
+    while intentos < 3:
+        usuario_nuevo = input("Ingrese su usuario: ")
+        
+        if usuario_nuevo.isalnum() and len(usuario_nuevo) >= 5:
+            nombre = input("Ingrese su nombre: ")
+            while True:
+                pin_nuevo = getpass.getpass("Ingrese su pin: ")
+                if len(pin_nuevo) >= 6 and pin_nuevo.isdigit():
+                    print("Su pin es correcto")
+                    deposito_minimo = valor_minimo()
+                    intentos_deposito = 0
+                    while intentos_deposito < 3:
+                        deposito = input("Ingrese su deposito: ")
+                        moneda = input("Ingrese su moneda: ")
+                        if  moneda == dolares:    
+                            if deposito >= valor_minimo:
+                                print(f"Deposito correcto: ",{deposito})
+                                break
+                            else:
+                                print("Ingrese un monto correcto")
+                        elif moneda ==  colones:
 
-ids_usuarios = set()
+
+
+
+                    break
+                else:
+                    print("¡Intntelo de nuevo!")
+            print("Usuario Ingresado")
+            print("¡Registro Exitoso!")
+            print("Bienvendo a DreamWorld Casino")
+            return usuario_nuevo
+        else:
+            print("Ingrese un usuario Alphanumerico y de 5 caracteres")
+            intentos += 1
+    
+    print("Has exedido todo los intentos, \n Regresando al menú principal")
+
+usuarios_registrados = set()
 
 while True:
-    id_ususario = obtener_id()
-    if id_ususario in ids_usuarios:
-        print("El ID ya existe, Ingrese otro.")
+    usuario_nuevo = registro_usuario()
+    if usuario_nuevo in usuarios_registrados:
+        print("El ususario ya existe, Ingrese otro.")
     else:
-        ids_usuarios.add(id_ususario)
-print("Su ID es: ")
-for id in ids_usuarios:
-    print(id)
-    
+        usuarios_registrados.add(usuario_nuevo)
+        print("Su Usuario es: ")
+    for usuario_nuevo in usuarios_registrados:
+        print(usuario_nuevo)
+
+
+registro_ususario()
+
